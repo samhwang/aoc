17fc79e0f75c3dd93e33f9d831d294a81547048c
Sam Huynh <samhwang2112.dev@gmail.com>
2022-12-07 09:26:25 +1100

    Reformat Day4

--- day4/task.ts
+++ day4/task.ts
@@ -12,41 +12,26 @@ const pairs = lines.reduce<[ElfTask, ElfTask][]>((accumulator, line) => {
   return accumulator;
 }, []);
 
+// TASK 1: Find all consumed task pairs (i.e. 1 is completely overlapped by the other)
 function findConsumed(elf1: ElfTask, elf2: ElfTask) {
   const firstConsumeLast = elf1[0] <= elf2[0] && elf1[1] >= elf2[1];
   const lastConsumeFirst = elf2[0] <= elf1[0] && elf2[1] >= elf1[1];
-  const overlap = firstConsumeLast || lastConsumeFirst;
-  return overlap;
+  return firstConsumeLast || lastConsumeFirst;
 }
 
-// TASK 1: Find all consumed task pairs (i.e. 1 is completely overlapped by the other)
-console.log('RUNNING TASK 1');
+console.log('TASK 1');
 const consumed = pairs.reduce((count, [elf1, elf2]) => {
-  const consumed = findConsumed(elf1, elf2);
-  return consumed ? count + 1 : count;
+  return findConsumed(elf1, elf2) ? count + 1 : count;
 }, 0);
 console.log({ consumed });
 
+// TASK 2: Find all overlapped pairs
 function findOverlap(elf1: ElfTask, elf2: ElfTask) {
-  if (findConsumed(elf1, elf2)) {
-    return true;
-  }
-
-  if (elf1[1] < elf2[0]) {
-    return false;
-  }
-
-  if (elf1[0] > elf2[1]) {
-    return false;
-  }
-
-  return true;
+  return !(elf1[1] < elf2[0] || elf1[0] > elf2[1]);
 }
 
-// TASK 2: Find all overlapped pairs
-console.log('RUNNING TASK 2');
+console.log('TASK 2');
 const overlap = pairs.reduce((count, [elf1, elf2]) => {
-  const overlap = findOverlap(elf1, elf2);
-  return overlap ? count + 1 : count;
+  return findOverlap(elf1, elf2) ? count + 1 : count;
 }, 0);
 console.log({ overlap });
