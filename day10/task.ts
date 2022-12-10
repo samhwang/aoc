467bd677e4ffd42f597c025053a715c6c67e1ba4
Sam Huynh <samhwang2112.dev@gmail.com>
2022-12-10 23:48:34 +1100

    Add task2 for day 10

--- day10/task.ts
+++ day10/task.ts
@@ -2,7 +2,15 @@ type Instruction = 'noop' | `addx ${number}`;
 
 const program = Deno
   .readTextFileSync('./input.txt')
-  .split('\n') as Instruction[];
+  .split('\n')
+  .map((line) => {
+    if (line.indexOf('noop') > -1) {
+      return [line];
+    }
+
+    return ['noop', line];
+  })
+  .flat() as Instruction[];
 
 // Task 1: Calculate signal strength
 const START_REGISTRY = 1;
@@ -21,14 +29,6 @@ function calculateSignalStrength(cycleCount: number, signal: number) {
 }
 
 program
-  .map((line) => {
-    if (line.indexOf('noop') > -1) {
-      return [line];
-    }
-
-    return ['noop', line];
-  })
-  .flat()
   .forEach((instruction) => {
     const signal = calculateSignalStrength(cycleCount, currentRegistry);
     signals.push(signal);
@@ -47,3 +47,36 @@ const totalStrengths = [20, 60, 100, 140, 180, 220].reduce((accum, index) => {
 }, 0);
 console.log('TASK 1: ', { totalStrengths });
 
+// Task 2: Render CRT from the input
+const MAX_WIDTH = 40;
+const MAX_HEIGHT = 6;
+const PIXEL = {
+  lit: '#',
+  nonLit: '.',
+};
+
+function getSpriteLocation(registry: number) {
+  return [registry - 1, registry, registry + 1];
+}
+
+const screen = Array(MAX_HEIGHT).fill(undefined).map(() => Array(MAX_WIDTH).fill(''));
+let currentRow = 0;
+let currentScan = 0;
+currentRegistry = START_REGISTRY;
+program
+  .forEach((instruction) => {
+    const spriteLocation = getSpriteLocation(currentRegistry);
+    screen[currentRow][currentScan] = spriteLocation.includes(currentScan) ? PIXEL.lit : PIXEL.nonLit;
+    currentRow = currentScan + 1 < MAX_WIDTH ? currentRow : currentRow + 1;
+    currentScan = currentScan + 1 < MAX_WIDTH ? currentScan + 1 : 0;
+
+    if (instruction === 'noop') {
+      return;
+    }
+
+    const [_, registryAsString] = instruction.split(' ');
+    const registryToAdd = Number.parseInt(registryAsString, 10);
+    currentRegistry = currentRegistry + registryToAdd;
+  });
+console.log('Task 2: ');
+screen.forEach((line) => console.log(line.join('')));
