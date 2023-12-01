541fcf796d2f5d2697cc6bcc7b7101793c08eeab
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-01 20:35:27 +1100

    d1: complete day 1 task 2

--- 2023/day1/task.ts
+++ 2023/day1/task.ts
@@ -35,6 +35,70 @@ function part1(input: string[]): number {
   }, 0);
 }
 
+const DIGIT_TO_NUMBER = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function findStartingDigit(line: string): { text: string; digit: number } {
+  let digit = 0;
+  let text = '';
+
+  for (const key of Object.keys(DIGIT_TO_NUMBER)) {
+    if (line.startsWith(key)) {
+      digit = DIGIT_TO_NUMBER[key];
+      text = key;
+      break;
+    }
+  }
+
+  return {
+    text,
+    digit,
+  };
+}
+
+function getNumberFromLine2(line: string): number {
+  const digits: string[] = [];
+  let lineClone = line;
+  const currentIndex = 0;
+
+  while (currentIndex < lineClone.length) {
+    if (charIsNumber(lineClone[currentIndex])) {
+      digits.push(lineClone.charAt(currentIndex));
+      lineClone = lineClone.slice(1);
+      continue;
+    }
+
+    const { text, digit } = findStartingDigit(lineClone);
+    if (digit > 0 && text !== '') {
+      digits.push(`${digit}`);
+      lineClone = lineClone.slice(1);
+      continue;
+    }
+
+    lineClone = lineClone.slice(1);
+  }
+
+  return Number.parseInt(`${digits[0]}${digits[digits.length - 1]}`, 10);
+}
+
+function part2(input: string[]): number {
+  return input.reduce((acc, line) => {
+    if (line.length === 0) {
+      return acc;
+    }
+    return acc + getNumberFromLine2(line);
+  }, 0);
+}
+
 async function go() {
   const input = parseInput();
 
