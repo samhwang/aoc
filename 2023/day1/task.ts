cb1932bf5222e4da53392256f433aa9d7e8ae8ac
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-02 06:27:57 +1100

    d1: simplify logic

--- 2023/day1/task.ts
+++ 2023/day1/task.ts
@@ -5,18 +5,21 @@ function charIsNumber(char: string): boolean {
 }
 
 function getNumberFromLine1(line: string): number {
-  const digits: string[] = [];
-  let lineClone = line;
-  const currentIndex = 0;
+  let first = '';
+  let last = '';
 
-  while (currentIndex < lineClone.length) {
-    if (charIsNumber(lineClone[currentIndex])) {
-      digits.push(lineClone.charAt(currentIndex));
+  for (let index = 0; index < line.length; index++) {
+    if (charIsNumber(line[index])) {
+      const digit = line[index];
+      if (!first) {
+        first = digit;
+      }
+
+      last = digit;
     }
-    lineClone = lineClone.slice(1);
   }
 
-  return Number.parseInt(`${digits[0]}${digits[digits.length - 1]}`, 10);
+  return Number.parseInt(`${first}${last}`, 10);
 }
 
 function part1(input: string[]): number {
@@ -29,58 +32,56 @@ function part1(input: string[]): number {
 }
 
 const DIGIT_TO_NUMBER = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-};
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+} as const;
 
-function findStartingDigit(line: string): { text: string; digit: number } {
-  let digit = 0;
-  let text = '';
+function findStartingDigit(line: string): string {
+  let digit = '';
 
   for (const key of Object.keys(DIGIT_TO_NUMBER)) {
     if (line.startsWith(key)) {
       digit = DIGIT_TO_NUMBER[key];
-      text = key;
       break;
     }
   }
 
-  return {
-    text,
-    digit,
-  };
+  return digit;
 }
 
 function getNumberFromLine2(line: string): number {
-  const digits: string[] = [];
-  let lineClone = line;
-  const currentIndex = 0;
+  let first = '';
+  let last = '';
 
-  while (currentIndex < lineClone.length) {
-    if (charIsNumber(lineClone[currentIndex])) {
-      digits.push(lineClone.charAt(currentIndex));
-      lineClone = lineClone.slice(1);
+  for (let index = 0; index < line.length; index++) {
+    if (charIsNumber(line[index])) {
+      const digit = line[index];
+      if (!first) {
+        first = digit;
+      }
+
+      last = digit;
       continue;
     }
 
-    const { text, digit } = findStartingDigit(lineClone);
-    if (digit > 0 && text !== '') {
-      digits.push(`${digit}`);
-      lineClone = lineClone.slice(1);
-      continue;
-    }
+    const digit = findStartingDigit(line.substring(index));
+    if (digit) {
+      if (!first) {
+        first = digit;
+      }
 
-    lineClone = lineClone.slice(1);
+      last = digit;
+    }
   }
 
-  return Number.parseInt(`${digits[0]}${digits[digits.length - 1]}`, 10);
+  return Number.parseInt(`${first}${last}`, 10);
 }
 
 function part2(input: string[]): number {
