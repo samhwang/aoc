09a04333985a8d886beda2481f7d701889c367b0
Sam Huynh <sam.huynh@lyka.com.au>
2023-12-02 18:26:30 +1100

    d2: WIP

--- 2023/day2/task.ts
+++ 2023/day2/task.ts
@@ -1,8 +1,39 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+type GameSet = {
+  red: number;
+  green: number;
+  blue: number;
+};
 
-function part2(input: string[]) {}
+type Color = 'red' | 'green' | 'blue';
+type Cubes = `${number} ${Color} `
+
+function isGameValid1(line: string): boolean {
+  let isValid = true;
+
+  const sets = line.substring(line.indexOf(': ') + 1).split(';');
+  for (const set of sets) {
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+    const grabs: Cubes[] = set.split(',');
+  }
+
+  return isValid;
+}
+
+function part1(input: string[]): number {
+  const MAX_RED = 12;
+  const MAX_GREEN = 13;
+  const MAX_BLUE = 14;
+
+  return 0;
+}
+
+function part2(input: string[]): number {
+  return 0;
+}
 
 function go() {
   const input = parseInput('./input.txt');
