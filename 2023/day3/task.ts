b44b70992ba219e44b8bf36c154574f942b39971
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-03 21:40:42 +1100

    d3: WIP task 2

--- 2023/day3/task.ts
+++ 2023/day3/task.ts
@@ -76,10 +76,51 @@ function part1(map: string[]): number {
   return sum;
 }
 
-function part2(input: string[]) {}
+const GEAR = '*';
+
+function calculateGearRatio(map: string[], row: number, col: number): number {
+  const MAX_COL = map[0].length - 1;
+  const MAX_ROW = map.length - 1;
+  let ratio = 1;
+  const coordinates: [number, number][] = [];
+
+  for (let r = row - 1; r <= row + 1; r++) {
+    if (r < 0 || r > MAX_ROW) {
+      continue;
+    }
+
+    for (let c = col - 1; c <= col + 1; c++) {
+      if (c < 0 || c > MAX_COL) {
+        continue;
+      }
+      if (isNumber(map[r][c])) {
+        coordinates.push([r, c])
+      }
+    }
+  }
+
+  console.log({ coordinates });
+}
+
+function part2(map: string[]): number {
+  let sum = 0;
+
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[row].length; col++) {
+      const char = map[row][col];
+      if (char !== GEAR) {
+        continue;
+      }
+
+      calculateGearRatio(map, row, col);
+    }
+  }
+
+  return sum;
+}
 
 function go() {
-  const input = parseInput('./input.txt');
+  const input = parseInput('./sample.txt');
 
   const res1 = part1(input);
   console.log('PART 1: ', res1);
