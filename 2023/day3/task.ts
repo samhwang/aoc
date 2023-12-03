ea36a054d78ddc965d1b1cdb1f4b63f9e3e1e482
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-03 19:07:08 +1100

    d3: finish task 1

--- 2023/day3/task.ts
+++ 2023/day3/task.ts
@@ -1,6 +1,80 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+const DOT = '.';
+
+function isNumber(text: string): boolean {
+  return !Number.isNaN(Number.parseInt(text, 10));
+}
+
+function isNotDot(text: string): boolean {
+  return text !== DOT;
+}
+
+function isValidSymbol(text: string): boolean {
+  return !!text && !isNumber(text) && isNotDot(text);
+}
+
+function isAdjacentToSymbol(map: string[], row: number, col: number): boolean {
+  const MAX_COL = map[0].length - 1;
+  const MAX_ROW = map.length - 1;
+  let valid = false;
+
+  for (let r = row - 1; r <= row + 1; r++) {
+    if (r < 0 || r > MAX_ROW) {
+      continue;
+    }
+
+    for (let c = col - 1; c <= col + 1; c++) {
+      if (c < 0 || c > MAX_COL) {
+        continue;
+      }
+      valid = valid || isValidSymbol(map[r][c]);
+    }
+  }
+
+  return valid;
+}
+
+function part1(map: string[]): number {
+  let sum = 0;
+  let numberString = '';
+  let adjacentToSymbol = false;
+
+  for (let row = 0; row < map.length; row++) {
+    const line = map[row];
+
+    if (line === '') {
+      break;
+    }
+
+    for (let col = 0; col < line.length; col++) {
+      const char = line[col];
+      if (!isNumber(char)) {
+        if (numberString !== '' && adjacentToSymbol) {
+          sum += Number.parseInt(numberString, 10);
+        }
+        numberString = '';
+        adjacentToSymbol = false;
+        continue;
+      }
+
+      if (isNumber(char)) {
+        numberString += char;
+        adjacentToSymbol = adjacentToSymbol || isAdjacentToSymbol(map, row, col);
+      }
+
+      if (col === line.length - 1) {
+        if (numberString !== '' && adjacentToSymbol) {
+          sum += Number.parseInt(numberString, 10);
+        }
+        numberString = '';
+        adjacentToSymbol = false;
+      }
+    }
+  }
+
+  return sum;
+}
 
 function part2(input: string[]) {}
 
