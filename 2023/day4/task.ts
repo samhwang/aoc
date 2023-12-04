11c8f049ea970d082c48ab3e012b496532498ca5
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-04 17:14:09 +1100

    d4: done p1

--- 2023/day4/task.ts
+++ 2023/day4/task.ts
@@ -1,6 +1,43 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+function calculateGameScore(card: string): number {
+  // Format: Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+  const [winningNumbers, cardNumbers] = card
+    .substring(card.indexOf(':') + 2)
+    .split('|')
+    .map((numbers) => {
+      return numbers
+        .trim()
+        .split(' ')
+        .filter((num) => num !== '')
+        .map((num) => parseInt(num.trim(), 10));
+    });
+
+  let score = 0;
+  for (const num of cardNumbers) {
+    if (!winningNumbers.includes(num)) {
+      continue;
+    }
+
+    if (!score) {
+      score = 1;
+      continue;
+    }
+    score = !score ? 1 : score * 2;
+  }
+
+  return score;
+}
+
+function part1(input: string[]): number {
+  let totalScore = 0;
+  for (const card of input) {
+    if (card === '') continue;
+    totalScore += calculateGameScore(card);
+  }
+
+  return totalScore;
+}
 
 function part2(input: string[]) {}
 
