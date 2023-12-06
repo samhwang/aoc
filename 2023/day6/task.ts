9f191db4f2ad7fca775056d6053d77447c13ea9b
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-06 21:57:07 +1100

    d6: WIP p1

--- 2023/day6/task.ts
+++ 2023/day6/task.ts
@@ -1,11 +1,35 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+type Race = { maxTime: number; recordDistance: number };
+function parseRace(input: string[]) {
+  const timeLine = input[0]
+    .substring(input[0].indexOf(':') + 1)
+    .split(' ')
+    .filter((item) => item !== '')
+    .map((time) => Number.parseInt(time, 10));
+  const distanceLine = input[1]
+    .substring(input[1].indexOf(':') + 1)
+    .split(' ')
+    .filter((item) => item !== '')
+    .map((distance) => Number.parseInt(distance, 10));
+
+  const raceInput: Race[] = [];
+  for (let i = 0; i < timeLine.length; i++) {
+    raceInput.push({ maxTime: timeLine[i], recordDistance: distanceLine[i] });
+  }
+
+  return raceInput;
+}
+
+function part1(input: string[]) {
+  const raceInput = parseRace(input);
+  console.log({ raceInput });
+}
 
 function part2(input: string[]) {}
 
 function go() {
-  const input = parseInput('./input.txt');
+  const input = parseInput('./sample.txt');
 
   const res1 = part1(input);
   console.log('PART 1: ', res1);
