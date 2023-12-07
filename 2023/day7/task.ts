6b01bd0509c690057728c1b55d1eaed2ff47b059
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-08 08:40:57 +1100

    d7: WIP p2

--- 2023/day7/task.ts
+++ 2023/day7/task.ts
@@ -27,6 +27,7 @@ const HAND_SCORE = {
   HIGH_CARD: 0,
 } as const;
 
+// Cards are now aligned with CARDS1, with no wild card
 function calculateHandScore1(cards: Hand['cards']): (typeof HAND_SCORE)[keyof typeof HAND_SCORE] {
   const cardCounts = cards.reduce(
     (acc, card) => {
@@ -99,6 +100,9 @@ function part1(input: string[]) {
       }
       return compare;
     });
+  scores.forEach((score) => {
+    console.log(score);
+  });
 
   const totalWin = scores.reduce((acc, hand, index) => {
     const result = acc + hand.bid * (index + 1);
@@ -108,7 +112,134 @@ function part1(input: string[]) {
   return totalWin;
 }
 
-function part2(input: string[]) {}
+// Cards are now aligned with CARDS2, with J as a wild card
+function calculateHandScore2(cards: Hand['cards']): (typeof HAND_SCORE)[keyof typeof HAND_SCORE] {
+  const cardCounts = cards.reduce(
+    (acc, card) => {
+      acc[card] = acc[card] ? acc[card] + 1 : 1;
+      return acc;
+    },
+    { J: 0 } as Record<Card, number>
+  );
+
+  const countJ = cardCounts.J;
+  if (countJ === 5 || countJ === 4) {
+    // If we have 5Js, it's already a 5 of a kind.
+    // If we have 4Js, it can be paired with another card to become 5 of a kind.
+    return HAND_SCORE.FIVE_OF_A_KIND;
+  }
+
+  if (countJ === 3) {
+    // If we have 3Js,...
+    const scoreWithoutJs = calculateHandScore1(cards.filter((card) => card !== 'J'));
+    if (scoreWithoutJs === HAND_SCORE.HIGH_CARD) {
+      // We can pair with a high card to make 4 of a kind
+      return HAND_SCORE.FOUR_OF_A_KIND;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.ONE_PAIR) {
+      // Or a pair to make 5 of a kind
+      return HAND_SCORE.FIVE_OF_A_KIND;
+    }
+
+    throw new Error('Unexpected score');
+  }
+
+  if (countJ === 2) {
+    // If we have 2Js,...
+    const scoreWithoutJs = calculateHandScore1(cards.filter((card) => card !== 'J'));
+    if (scoreWithoutJs === HAND_SCORE.HIGH_CARD) {
+      // We can pair with a high card to make 3 of a kind
+      return HAND_SCORE.THREE_OF_A_KIND;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.ONE_PAIR) {
+      // Or a pair to make 4 of a kind
+      return HAND_SCORE.FOUR_OF_A_KIND;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.THREE_OF_A_KIND) {
+      // Or a 3 of a kind to make 5 of a kind
+      return HAND_SCORE.FIVE_OF_A_KIND;
+    }
+
+    throw new Error('Unexpected score');
+  }
+
+  if (countJ === 1) {
+    // If we have 1J,...
+    const scoreWithoutJs = calculateHandScore1(cards.filter((card) => card !== 'J'));
+    if (scoreWithoutJs === HAND_SCORE.HIGH_CARD) {
+      // We can pair with a high card to make 2 pairs
+      return HAND_SCORE.TWO_PAIRS;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.ONE_PAIR) {
+      // Or a pair to make 3 of a kind
+      return HAND_SCORE.THREE_OF_A_KIND;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.TWO_PAIRS) {
+      // Or a 2 pairs to make full house
+      return HAND_SCORE.FULL_HOUSE;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.THREE_OF_A_KIND) {
+      // Or a 3 of a kind to make 4 of a kind
+      return HAND_SCORE.FOUR_OF_A_KIND;
+    }
+
+    if (scoreWithoutJs === HAND_SCORE.FOUR_OF_A_KIND) {
+      // Or a 4 of a kind to make 5 of a kind
+      return HAND_SCORE.FIVE_OF_A_KIND;
+    }
+
+    throw new Error('Unexpected score');
+  }
+
+  // we have no Js here.
+  return calculateHandScore1(cards);
+}
+
+function part2(input: string[]) {
+  const hands = parseHands(input);
+
+  const scores = hands
+    .map((hand) => {
+      const score = calculateHandScore2(hand.cards);
+      return { ...hand, score };
+    })
+    .sort((a, b) => {
+      if (a.score !== b.score) {
+        return a.score - b.score;
+      }
+
+      let compare = 0;
+      for (let i = 0; i < 5; i++) {
+        if (a.cards[i] === b.cards[i]) {
+          continue;
+        }
+
+        const cardA = a.cards[i];
+        const indexA = CARDS2.indexOf(cardA);
+        const cardB = b.cards[i];
+        const indexB = CARDS2.indexOf(cardB);
+        compare = indexA - indexB;
+        break;
+      }
+      return compare;
+    });
+  scores.forEach((score) => {
+    console.log(score);
+  });
+
+  const totalWin = scores.reduce((acc, hand, index) => {
+    const result = acc + hand.bid * (index + 1);
+
+    return result;
+  }, 0);
+  return totalWin;
+}
 
 function go() {
   const input = parseInput('./input.txt');
