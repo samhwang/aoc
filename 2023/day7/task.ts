f4e827f4519e8a1a4e06a7c6d18e2c6376278a9a
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-07 21:02:33 +1100

    d7: WIP p1

--- 2023/day7/task.ts
+++ 2023/day7/task.ts
@@ -1,11 +1,38 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+type Hand = { cards: string[]; bid: number };
+function parseHands(input: string[]): Hand[] {
+  return input
+    .filter((line) => line !== '')
+    .map((line) => {
+      const [cards, bid] = line.split(' ');
+
+      return {
+        cards: cards.split('').sort(),
+        bid: Number.parseInt(bid, 10),
+      };
+    });
+}
+
+const HAND_SCORE = {
+  FIVE_OF_A_KIND: 6,
+  FOUR_OF_A_KIND: 5,
+  FULL_HOUSE: 4,
+  THREE_OF_A_KIND: 3,
+  TWO_PAIRS: 2,
+  ONE_PAIR: 1,
+  HIGH_CARD: 0,
+} as const;
+
+function part1(input: string[]) {
+  const hands = parseHands(input);
+  console.log(hands);
+}
 
 function part2(input: string[]) {}
 
 function go() {
-  const input = parseInput('./input.txt');
+  const input = parseInput('./sample.txt');
 
   const res1 = part1(input);
   console.log('PART 1: ', res1);
