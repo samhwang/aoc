f4c062003d254e36fe50162a8ca5915008d43f97
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-01 22:18:28 +1100

    2024-d1: done p1

--- 2024/day1/task.ts
+++ 2024/day1/task.ts
@@ -1,6 +1,34 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+function parseIntoList(input: string[]): [number[], number[]] {
+  return input.reduce(
+    ([leftList, rightList], line) => {
+      if (line.trim().length === 0) {
+        return [leftList, rightList];
+      }
+
+      const [left, right] = line.split('  ').map((char) => Number.parseInt(char, 10));
+      leftList.push(left);
+      rightList.push(right);
+      return [leftList, rightList];
+    },
+    [[], []] as [number[], number[]]
+  );
+}
+
+// PART 1: Calculate distance between ascending pairs
+
+function part1(input: string[]) {
+  const [leftList, rightList] = parseIntoList(input);
+  leftList.sort((a, b) => a - b);
+  rightList.sort((a, b) => a - b);
+
+  let output = 0;
+  for (let i = 0; i < leftList.length; i++) {
+    output += Math.abs(leftList[i] - rightList[i]);
+  }
+  return output;
+}
 
 function part2(input: string[]) {}
 
