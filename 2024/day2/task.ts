0d342c7f6e4f5a9b1ec58b2b7c37120fcb15c5ef
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-03 00:19:10 +1100

    2024-d2: done p2

--- 2024/day2/task.ts
+++ 2024/day2/task.ts
@@ -60,7 +60,45 @@ function part1(input: string[]): number {
   }, 0);
 }
 
-function part2(input: string[]) {}
+/**
+ * p2: Same rule as part 1, but now with a dampener:
+ * The dampener can remove 1 score to make the whole report safe again.
+ */
+
+function isFixable(report: number[]): boolean {
+  const count = report.reduce((count, _score, index) => {
+    const reportWithoutElem = [...report.slice(0, index), ...report.slice(index + 1)];
+    const fixable = isSafeReport(reportWithoutElem);
+    if (!fixable) {
+      return count;
+    }
+    return count + 1;
+  }, 0);
+
+  return count > 0;
+}
+
+function part2(input: string[]): number {
+  return input.reduce((accum, line) => {
+    if (line.trim().length === 0) {
+      return accum;
+    }
+
+    const report = line.split(' ').map((score) => Number.parseInt(score, 10));
+    const isIncreasingOrDecreasing = isIncreasing(report) || isDecreasing(report);
+    const withinDelta = reportWithinDelta(report);
+    if (isIncreasingOrDecreasing && withinDelta) {
+      return accum + 1;
+    }
+
+    const fixable = isFixable(report);
+    if (fixable) {
+      return accum + 1;
+    }
+
+    return accum;
+  }, 0);
+}
 
 function go() {
   const input = parseInput('./input.txt');
