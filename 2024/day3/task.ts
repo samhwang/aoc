10c75023a0170cd6e5d3bb6350c9dbc1e17548ef
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-03 23:12:05 +1100

    2024-d3: done p2

--- 2024/day3/task.ts
+++ 2024/day3/task.ts
@@ -26,7 +26,43 @@ function part1(input: string[]): number {
   return total;
 }
 
-function part2(input: string[]) {}
+/**
+ * p2: Same rule as d1, but now with `do()` and `don't()` instructions.
+ * The `do()` will enable the next calculations, and the `don't()` will stop it.
+ */
+type DoText = `do()`;
+type DontText = `don't()`;
+function part2(input: string[]) {
+  const doDontMulRegex = /((mul)\((\d{1,3})\,(\d{1,3})\))|(do)\(\)|(don\'t)\(\)/g;
+
+  let addToTotal = true;
+  let total = 0;
+  for (const line of input) {
+    const matches = line.matchAll(doDontMulRegex);
+    const instructions = [...matches].map((val) => val[0]) as (MulText | DoText | DontText)[];
+
+    for (const instruction of instructions) {
+      if (instruction === 'do()') {
+        addToTotal = true;
+        continue;
+      }
+
+      if (instruction === "don't()") {
+        addToTotal = false;
+        continue;
+      }
+
+      if (!addToTotal) {
+        continue;
+      }
+
+      const subtotal = calculateMul(instruction);
+      total += subtotal;
+    }
+  }
+
+  return total;
+}
 
 function go(): void {
   const input = parseInput('./input.txt');
