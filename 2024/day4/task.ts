2060a8b3138812a8810c39e4f6c8663b726114c8
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-04 17:12:18 +1100

    2024-d4: refactor to use a direction map and iterator

--- 2024/day4/task.ts
+++ 2024/day4/task.ts
@@ -1,25 +1,63 @@
 import { parseInput } from '../src/parse-input';
 
 type Coordinates = [number, number];
+type Direction = 'N' | 'S' | 'E' | 'W' | 'NW' | 'NE' | 'SE' | 'SW';
 
-const WORD_TO_FIND = 'XMAS';
+const DIRECTION_MAP: Record<Direction, Coordinates> = {
+  N: [-1, 0],
+  NW: [-1, -1],
+  NE: [-1, +1],
+  W: [0, -1],
+  E: [0, 1],
+  S: [1, 0],
+  SW: [1, -1],
+  SE: [1, 1],
+};
+
+function iterateCoordinates([y, x]: Coordinates, direction: Direction, times: number): Coordinates {
+  const [plusY, plusX] = DIRECTION_MAP[direction];
+  return [y + plusY * times, x + plusX * times];
+}
+
+function formWordOnGrid([y, x]: Coordinates, direction: Direction, wordLength: number, grid: string[][]): string {
+  try {
+    let word = grid[y][x];
+
+    for (let i = 1; i <= wordLength; i++) {
+      const [newY, newX] = iterateCoordinates([y, x], direction, i);
+      word = `${word}${grid[newY][newX]}`;
+    }
+
+    return word;
+  } catch (error) {
+    console.log('ERROR: Out of boundary', {
+      coordinates: [y, x],
+      direction,
+      wordLength,
+    });
+    return '';
+  }
+}
 
 function parseIntoGrid(input: string[]): string[][] {
   return input.map((line) => line.split(''));
 }
 
+/** p1: Count all the XMAS words that occured in the word grid */
 function countXmasWords([y, x]: Coordinates, grid: string[][]): number {
+  const WORD_TO_FIND = 'XMAS';
+  const LENGTH = WORD_TO_FIND.length - 1;
   const MAX_X = grid.length - 1;
   const MAX_Y = grid[0].length - 1;
 
-  const hasXmasN = y >= 3 && `${grid[y][x]}${grid[y - 1][x]}${grid[y - 2][x]}${grid[y - 3][x]}` === WORD_TO_FIND;
-  const hasXmasNW = y >= 3 && x >= 3 && `${grid[y][x]}${grid[y - 1][x - 1]}${grid[y - 2][x - 2]}${grid[y - 3][x - 3]}` === WORD_TO_FIND;
-  const hasXmasNE = y >= 3 && x <= MAX_X - 3 && `${grid[y][x]}${grid[y - 1][x + 1]}${grid[y - 2][x + 2]}${grid[y - 3][x + 3]}` === WORD_TO_FIND;
-  const hasXmasW = x >= 3 && `${grid[y][x]}${grid[y][x - 1]}${grid[y][x - 2]}${grid[y][x - 3]}` === WORD_TO_FIND;
-  const hasXmasE = x <= MAX_X - 3 && `${grid[y][x]}${grid[y][x + 1]}${grid[y][x + 2]}${grid[y][x + 3]}` === WORD_TO_FIND;
-  const hasXmasS = y <= MAX_Y - 3 && `${grid[y][x]}${grid[y + 1][x]}${grid[y + 2][x]}${grid[y + 3][x]}` === WORD_TO_FIND;
-  const hasXmasSW = y <= MAX_Y - 3 && x >= 3 && `${grid[y][x]}${grid[y + 1][x - 1]}${grid[y + 2][x - 2]}${grid[y + 3][x - 3]}` === WORD_TO_FIND;
-  const hasXmasSE = y <= MAX_Y - 3 && x <= MAX_X - 3 && `${grid[y][x]}${grid[y + 1][x + 1]}${grid[y + 2][x + 2]}${grid[y + 3][x + 3]}` === WORD_TO_FIND;
+  const hasXmasN = y >= LENGTH && formWordOnGrid([y, x], 'N', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasNW = y >= LENGTH && x >= LENGTH && formWordOnGrid([y, x], 'NW', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasNE = y >= LENGTH && x <= MAX_X - LENGTH && formWordOnGrid([y, x], 'NE', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasW = x >= 3 && formWordOnGrid([y, x], 'W', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasE = x <= MAX_X - LENGTH && formWordOnGrid([y, x], 'E', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasS = y <= MAX_Y - LENGTH && formWordOnGrid([y, x], 'S', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasSW = y <= MAX_Y - LENGTH && x >= LENGTH && formWordOnGrid([y, x], 'SW', LENGTH, grid) === WORD_TO_FIND;
+  const hasXmasSE = y <= MAX_Y - LENGTH && x <= MAX_X - LENGTH && formWordOnGrid([y, x], 'SE', LENGTH, grid) === WORD_TO_FIND;
 
   const checks = [hasXmasN, hasXmasNW, hasXmasNE, hasXmasW, hasXmasE, hasXmasS, hasXmasSW, hasXmasSE];
   const count = checks.reduce((count, check) => (check ? count + 1 : count), 0);
@@ -33,7 +71,6 @@ function part1(input: string[]): number {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[0].length; x++) {
       const currentChar = grid[y][x];
-      console.log({ currentChar });
       if (currentChar !== 'X') {
         continue;
       }
