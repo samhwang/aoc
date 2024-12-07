2371f6e3028865f19d9c7f92948f14972155c9b8
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-08 02:59:49 +1100

    2024-47 done p1

--- 2024/day7/task.ts
+++ 2024/day7/task.ts
@@ -1,6 +1,70 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+
+type Equation = {
+  result: number;
+  operands: number[];
+};
+function parseIntoEquation(line: string): Equation {
+  const matches = line.match(/(\d+)/g);
+
+  if (!matches) {
+    throw new Error('Invalid line!');
+  }
+
+  return {
+    result: Number.parseInt(matches[0], 10),
+    operands: matches.slice(1).map((v) => Number.parseInt(v, 10)),
+  };
+}
+
+/**
+ * p1: Find the equations that can add up to the number on the left.
+ */
+function checkIfAddUp1({ result, operands }: Equation): boolean {
+  let doesAddUp = false;
+  let scenarioId = 0;
+  const operandsCount = operands.length;
+  const operatorCount = operandsCount - 1;
+  const OPERATORS = ['+', '*'] as const;
+  const maxOperationLimit = OPERATORS.length ** operatorCount;
+
+  while (!doesAddUp && scenarioId <= maxOperationLimit) {
+    const operations = scenarioId.toString(2).padStart(operatorCount, '0');
+    const subtotal = operands.reduce((accumulator, num, index) => {
+      if (index === 0) {
+        return num;
+      }
+
+      const operationString = operations.slice(index - 1, index);
+      const operationNum = Number.parseInt(operationString, 10) as 0 | 1;
+      const operator = OPERATORS[operationNum];
+      switch (operator) {
+        case '*':
+          return accumulator * num;
+
+        case '+':
+          return accumulator + num;
+      }
+    }, 0);
+    doesAddUp = subtotal === result;
+    scenarioId += 1;
+  }
+
+  return doesAddUp;
+}
+
+function part1(input: string[]): number {
+  let totalSum = 0;
+  for (const line of input) {
+    const equation = parseIntoEquation(line);
+    const correct = checkIfAddUp1(equation);
+    if (correct) {
+      totalSum += equation.result;
+    }
+  }
+  return totalSum;
+}
 
 function part2(input: string[]) {}
 
