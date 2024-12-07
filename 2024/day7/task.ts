5666f7e218d2ebb60ad03aa2febd668b186bdce7
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-08 03:39:38 +1100

    2024-d7: refactor the checkifAddUp into a common function

--- 2024/day7/task.ts
+++ 2024/day7/task.ts
@@ -1,9 +1,5 @@
 import { parseInput } from '../src/parse-input';
 
-function joinNumbers(a: number, b: number): number {
-  return Number.parseInt(`${a}${b}`, 10);
-}
-
 type Equation = {
   result: number;
   operands: number[];
@@ -21,100 +17,75 @@ function parseIntoEquation(line: string): Equation {
   };
 }
 
+type Operations = Record<string, (a: number, b: number) => number>;
+
+function joinNumbers(a: number, b: number): number {
+  return Number.parseInt(`${a}${b}`, 10);
+}
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+function multiplyNumbers(a: number, b: number): number {
+  return a * b;
+}
+
 /**
  * p1: Find the equations that can add up to the number on the left.
  */
-function checkIfAddUp1({ result, operands }: Equation): boolean {
+function checkIfAddUp({ result, operands }: Equation, operations: Operations): boolean {
   let doesAddUp = false;
   let scenarioId = 0;
   const operandsCount = operands.length;
   const operatorCount = operandsCount - 1;
-  const OPERATORS = ['+', '*'] as const;
-  const maxOperationLimit = OPERATORS.length ** operatorCount;
+  const operatorSymbols = Object.keys(operations);
+  const operationCount = operatorSymbols.length;
+  const maxOperationLimit = operationCount ** operatorCount;
 
   while (!doesAddUp && scenarioId <= maxOperationLimit) {
-    const operations = scenarioId.toString(OPERATORS.length).padStart(operatorCount, '0');
-    const subtotal = operands.reduce((accumulator, num, index) => {
-      if (index === 0) {
-        return num;
-      }
-
-      const operationString = operations.slice(index - 1, index);
-      const operationNum = Number.parseInt(operationString, 10) as 0 | 1;
-      const operator = OPERATORS[operationNum];
-      switch (operator) {
-        case '*':
-          return accumulator * num;
-
-        case '+':
-          return accumulator + num;
-      }
-    }, 0);
+    const operationSequence = scenarioId.toString(operationCount).padStart(operatorCount, '0');
+    const subtotal = operands.slice(1).reduce((accumulator, num, index) => {
+      const operationString = operationSequence.slice(index, index + 1);
+      const operationNum = Number.parseInt(operationString, 10);
+      const operator = operatorSymbols[operationNum];
+      const operationFunc = operations[operator];
+      return operationFunc(accumulator, num);
+    }, operands[0]);
     doesAddUp = subtotal === result;
     scenarioId += 1;
   }
 
   return doesAddUp;
+}
+
+function checkReport(input: string[], operations: Operations): number {
+  let totalSum = 0;
+  for (const line of input) {
+    const equation = parseIntoEquation(line);
+    const correct = checkIfAddUp(equation, operations);
+    if (correct) {
+      totalSum += equation.result;
+    }
+  }
+  return totalSum;
 }
 
 function part1(input: string[]): number {
-  let totalSum = 0;
-  for (const line of input) {
-    const equation = parseIntoEquation(line);
-    const correct = checkIfAddUp1(equation);
-    if (correct) {
-      totalSum += equation.result;
-    }
-  }
-  return totalSum;
-}
-
-function checkIfAddUp2({ result, operands }: Equation): boolean {
-  let doesAddUp = false;
-  let scenarioId = 0;
-  const operandsCount = operands.length;
-  const operatorCount = operandsCount - 1;
-  const OPERATORS = ['+', '*', '||'] as const;
-  const maxOperationLimit = OPERATORS.length ** operatorCount;
-
-  while (!doesAddUp && scenarioId <= maxOperationLimit) {
-    const operations = scenarioId.toString(OPERATORS.length).padStart(operatorCount, '0');
-    const subtotal = operands.reduce((accumulator, num, index) => {
-      if (index === 0) {
-        return num;
-      }
-
-      const operationString = operations.slice(index - 1, index);
-      const operationNum = Number.parseInt(operationString, 10) as 0 | 1 | 2;
-      const operator = OPERATORS[operationNum];
-      switch (operator) {
-        case '*':
-          return accumulator * num;
-
-        case '+':
-          return accumulator + num;
-
-        case '||':
-          return joinNumbers(accumulator, num);
-      }
-    }, 0);
-    doesAddUp = subtotal === result;
-    scenarioId += 1;
-  }
-
-  return doesAddUp;
+  const operations: Operations = {
+    '+': addNumbers,
+    '*': multiplyNumbers,
+  };
+  return checkReport(input, operations);
 }
 
 function part2(input: string[]): number {
-  let totalSum = 0;
-  for (const line of input) {
-    const equation = parseIntoEquation(line);
-    const correct = checkIfAddUp2(equation);
-    if (correct) {
-      totalSum += equation.result;
-    }
-  }
-  return totalSum;
+  const operations: Operations = {
+    '+': addNumbers,
+    '*': multiplyNumbers,
+    '||': joinNumbers,
+  };
+  return checkReport(input, operations);
 }
 
 function go(): void {
