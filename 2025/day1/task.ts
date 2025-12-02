c8e23124a52c50c6397bb483d4ba43868e76c9ae
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-02 13:24:37 +1100

    2025-day1: refactor move out common constants

--- 2025/day1/task.ts
+++ 2025/day1/task.ts
@@ -1,59 +1,46 @@
 import { parseInput } from '../src/parse-input';
 
-function parseInstruction(instruction: string): { direction: 'L' | 'R'; steps: number } {
+type Direction = 'L' | 'R';
+
+function parseInstruction(instruction: string): { direction: Direction; steps: number } {
   const matches = instruction.match(/([LR])(\d+)/);
   if (!matches) throw new Error(`Invalid instruction: ${instruction}`);
-  const direction = matches[1] as 'L' | 'R';
+  const direction = matches[1] as Direction;
   const steps = parseInt(matches[2], 10);
   return { direction, steps };
 }
 
-function part1(input: string[]) {
-  const instructions = input.map(parseInstruction);
+const START = 50;
+const MIN = 0;
+const MAX = 99;
+const TOTAL = MAX + 1;
 
-  const START = 50;
-  const MIN = 0;
-  const MAX = 99;
+function part1(instructions: { direction: Direction; steps: number }[]) {
   let currentNumber = START;
   let stopsAt0 = 0;
 
   instructions.forEach(({ direction, steps }) => {
     for (let count = 0; count < steps; count++) {
       const move = direction === 'L' ? -1 : 1;
-      currentNumber += move;
-      if (currentNumber < MIN) {
-        currentNumber = MAX;
-      } else if (currentNumber > MAX) {
-        currentNumber = MIN;
-      }
+      currentNumber = (currentNumber + move) % TOTAL;
     }
 
-    if (currentNumber === 0) {
+    if (currentNumber === MIN) {
       stopsAt0++;
     }
   });
   return stopsAt0;
 }
 
-function part2(input: string[]) {
-  const instructions = input.map(parseInstruction);
-
-  const START = 50;
-  const MIN = 0;
-  const MAX = 99;
+function part2(instructions: { direction: Direction; steps: number }[]) {
   let currentNumber = START;
   let stopsAt0 = 0;
 
   instructions.forEach(({ direction, steps }) => {
     for (let count = 0; count < steps; count++) {
       const move = direction === 'L' ? -1 : 1;
-      currentNumber += move;
-      if (currentNumber < MIN) {
-        currentNumber = MAX;
-      } else if (currentNumber > MAX) {
-        currentNumber = MIN;
-      }
-      if (currentNumber === 0) {
+      currentNumber = (currentNumber + move) % TOTAL;
+      if (currentNumber === MIN) {
         stopsAt0++;
       }
     }
@@ -66,15 +53,16 @@ function go(): void {
 
   console.time('parse-input');
   const input = parseInput('./input.txt');
+  const instructions = input.map(parseInstruction);
   console.timeEnd('parse-input');
 
   console.time('part 1');
-  const res1 = part1(input);
+  const res1 = part1(instructions);
   console.log('PART 1: ', res1);
   console.timeEnd('part 1');
 
   console.time('part 2');
-  const res2 = part2(input);
+  const res2 = part2(instructions);
   console.log('PART 2: ', res2);
   console.timeEnd('part 2');
 
