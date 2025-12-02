3c848b49ddcdcebaffa7c37bab122b43630a672b
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-02 23:41:20 +1100

    2025-day2: done p2

--- 2025/day2/task.ts
+++ 2025/day2/task.ts
@@ -12,21 +12,33 @@ function part1(ranges: Range[]) {
   let total = 0;
   ranges.forEach(([start, end]) => {
     for (let num = start; num <= end; num++) {
-      const numAsString = num.toString()
+      const numAsString = num.toString();
       if (numAsString.length % 2 !== 0) {
         continue;
       }
-      const part1 = numAsString.slice(0, numAsString.length / 2)
-      const part2 = numAsString.slice(numAsString.length / 2)
+      const part1 = numAsString.slice(0, numAsString.length / 2);
+      const part2 = numAsString.slice(numAsString.length / 2);
       if (part1 === part2) {
-        total += num
+        total += num;
       }
     }
-  })
-  return total
+  });
+  return total;
 }
 
 function part2(ranges: Range[]) {
+  let total = 0;
+  ranges.forEach(([start, end]) => {
+    for (let num = start; num <= end; num++) {
+      const match = num.toString().match(/^(\d+?)\1+$/);
+      if (!match) {
+        continue;
+      }
+
+      total += num;
+    }
+  });
+  return total;
 }
 
 function go(): void {
@@ -43,7 +55,7 @@ function go(): void {
   console.timeEnd('part 1');
 
   console.time('part 2');
-  const res2 = part2(input);
+  const res2 = part2(ranges);
   console.log('PART 2: ', res2);
   console.timeEnd('part 2');
 
