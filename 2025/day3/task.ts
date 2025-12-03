b895b13707201518ff566bc453870e2e91b73654
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-03 22:32:38 +1100

    2025-day3: done p2 and refactor solution into a generic formula

--- 2025/day3/task.ts
+++ 2025/day3/task.ts
@@ -1,20 +1,38 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {
+function solve(input: string[], maxLength: number) {
   return input.reduce((totalJoltage, line) => {
     const batteries = line.split('');
-    let largestJoltage = 0;
-    for (let a = 0; a < batteries.length; a++) {
-      for (let b = a + 1; b < batteries.length; b++) {
-        const number = Number.parseInt(`${batteries[a]}${batteries[b]}`, 10);
-        largestJoltage = Math.max(largestJoltage, number);
-      }
+    let largestJoltage = '';
+    // method:
+    // - Find largest number first, it should be between index 0 and batteries.length - MAX_LENGTH
+    // - Cut the string from the index of the previous number
+    // - The next number should be the largest between index of the previous number and batteries.length - MAX_LENGTH + how_many_found_numbers
+    // e.g. with MAX_LENGTH = 2, and a line has 15 chars `818181911112111`:
+    // - Our first char searches between index 0 and 13, find the first index of the largest number (value: 9, index: 6).
+    // - Cut the string from index 6, so we now have `11112111`
+    // - Our second char searches between index 7 and the original index 14.
+    let searchArray = batteries;
+    while (largestJoltage.length < maxLength) {
+      const endScanRange = searchArray.length - maxLength + largestJoltage.length + 1;
+      const largestNumber = Math.max(...searchArray.slice(0, endScanRange).map((val) => Number.parseInt(val, 10)));
+      const largestNumIndex = searchArray.findIndex((val) => Number.parseInt(val, 10) === largestNumber);
+      searchArray = searchArray.slice(largestNumIndex + 1);
+      largestJoltage += largestNumber.toString();
     }
-    return totalJoltage + largestJoltage;
-  }, 0)
+    return totalJoltage + Number.parseInt(largestJoltage, 10);
+  }, 0);
 }
 
-function part2(input: string[]) {}
+function part1(input: string[]) {
+  const MAX_LENGTH = 2;
+  return solve(input, MAX_LENGTH);
+}
+
+function part2(input: string[]) {
+  const MAX_LENGTH = 12;
+  return solve(input, MAX_LENGTH);
+}
 
 function go(): void {
   console.time('task');
