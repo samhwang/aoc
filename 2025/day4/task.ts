4a154253d2b59966179f101a3858a793002de771
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-04 23:04:15 +1100

    2025-day4: done part1

--- 2025/day4/task.ts
+++ 2025/day4/task.ts
@@ -1,6 +1,44 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+const PAPER = '@';
+const MAX_ADJACENT_PAPER = 4;
+
+function buildMap(input: string[]): string[][] {
+  return input.map((line) => line.split(''));
+}
+
+function countAdjacentRolls(map: string[][], x: number, y: number) {
+  const nw = x > 0 && y > 0 ? map[y - 1][x - 1] : false;
+  const n = y > 0 ? map[y - 1][x] : false;
+  const ne = x < map[y].length - 1 && y > 0 ? map[y - 1][x + 1] : false;
+  const w = x > 0 ? map[y][x - 1] : false;
+  const e = x < map[y].length - 1 ? map[y][x + 1] : false;
+  const sw = x > 0 && y < map.length - 1 ? map[y + 1][x - 1] : false;
+  const s = y < map.length - 1 ? map[y + 1][x] : false;
+  const se = x < map[y].length - 1 && y < map.length - 1 ? map[y + 1][x + 1] : false;
+
+  const adjacentTiles = [nw, n, ne, w, e, sw, s, se];
+
+  return adjacentTiles.filter((tile) => tile === PAPER).length;
+}
+
+function part1(input: string[]) {
+  const map = buildMap(input);
+  let acc = 0;
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] !== PAPER) {
+        continue;
+      }
+
+      const count = countAdjacentRolls(map, x, y);
+      if (count < MAX_ADJACENT_PAPER ) {
+        acc++;
+      }
+    }
+  }
+  return acc;
+}
 
 function part2(input: string[]) {}
 
