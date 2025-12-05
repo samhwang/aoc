b34925cfd5e3a455ed7806da040dadc2cdc24b1d
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-05 22:32:55 +1100

    2025-day5: build out final ranges in the beginning
    An attempt to speed up task 1 run time, This shaved off like 3 ms.

--- 2025/day5/task.ts
+++ 2025/day5/task.ts
@@ -2,6 +2,10 @@ import { parseInput } from '../src/parse-input';
 
 type Range = [number, number];
 
+function checkOverlappingRanges([start1, end1]: Range, [start2, end2]: Range): boolean {
+  return start1 <= end2 && start2 <= end1;
+}
+
 function buildRange(input: string[]) {
   const freshRanges: Range[] = [];
   const ingredients: number[] = [];
@@ -19,25 +23,7 @@ function buildRange(input: string[]) {
 
     ingredients.push(Number.parseInt(line, 10));
   });
-  return {
-    freshRanges,
-    ingredients,
-  };
-}
 
-function part1(input: string[]) {
-  const { freshRanges, ingredients } = buildRange(input);
-  return ingredients.filter((ingredient) => {
-    return freshRanges.some(([start, end]) => ingredient >= start && ingredient <= end);
-  }).length;
-}
-
-function checkOverlappingRanges([start1, end1]: Range, [start2, end2]: Range): boolean {
-  return start1 <= end2 && start2 <= end1;
-}
-
-function part2(input: string[]) {
-  const { freshRanges } = buildRange(input);
   const finalRanges = freshRanges
     .sort(([start1], [start2]) => {
       return start1 - start2;
@@ -54,11 +40,25 @@ function part2(input: string[]) {
         accumulator.push(currentRange);
       }
       return accumulator;
-    }, [] as Range[])
-    .reduce((accumulator, [start, end]) => {
-      const numInRange = end - start + 1;
-      return accumulator + numInRange;
-    }, 0);
+    }, [] as Range[]);
+
+  return {
+    freshRanges: finalRanges,
+    ingredients,
+  };
+}
+
+function part1(freshRanges: Range[], ingredients: number[]) {
+  return ingredients.filter((ingredient) => {
+    return freshRanges.some(([start, end]) => ingredient >= start && ingredient <= end);
+  }).length;
+}
+
+function part2(freshRanges: Range[]) {
+  const finalRanges = freshRanges.reduce((accumulator, [start, end]) => {
+    const numInRange = end - start + 1;
+    return accumulator + numInRange;
+  }, 0);
   return finalRanges;
 }
 
@@ -67,15 +67,16 @@ function go(): void {
 
   console.time('parse-input');
   const input = parseInput('./input.txt');
+  const { freshRanges, ingredients } = buildRange(input);
   console.timeEnd('parse-input');
 
   console.time('part 1');
-  const res1 = part1(input);
+  const res1 = part1(freshRanges, ingredients);
   console.log('PART 1: ', res1);
   console.timeEnd('part 1');
 
   console.time('part 2');
-  const res2 = part2(input);
+  const res2 = part2(freshRanges);
   console.log('PART 2: ', res2);
   console.timeEnd('part 2');
 
