184e517833dd64d11cda47ff9542fa5861c2762a
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-05 21:49:47 +1100

    2025-day5: done p1

--- 2025/day5/task.ts
+++ 2025/day5/task.ts
@@ -1,6 +1,34 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+function buildRange(input: string[]) {
+  const freshRanges: [number, number][] = [];
+  const ingredients: number[] = [];
+
+  input.forEach((line) => {
+    if (line.length === 0) {
+      return;
+    }
+
+    if (line.includes('-')) {
+      const [start, end] = line.split('-').map((num) => Number.parseInt(num, 10)) as [number, number];
+      freshRanges.push([start, end]);
+      return;
+    }
+
+    ingredients.push(Number.parseInt(line, 10));
+  });
+  return {
+    freshRanges,
+    ingredients,
+  };
+}
+
+function part1(input: string[]) {
+  const { freshRanges, ingredients } = buildRange(input);
+  return ingredients.filter((ingredient) => {
+    return freshRanges.some(([start, end]) => ingredient >= start && ingredient <= end);
+  }).length;
+}
 
 function part2(input: string[]) {}
 
