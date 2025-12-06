6683177684ca8de2c6262eca599416ec28b93d2c
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-06 23:00:18 +1100

    2025-day6: done p2

--- 2025/day6/task.ts
+++ 2025/day6/task.ts
@@ -1,14 +1,21 @@
-import { parseInput } from '../src/parse-input';
+import fs from 'node:fs';
+
+function parseInput(inputPath: string) {
+  const input = fs.readFileSync(inputPath, { encoding: 'utf-8' });
+  return input.split('\n');
+}
+
+const PLUS = '+';
+const MULTIPLY = '*';
 
 function part1(input: string[]) {
-  const PLUS = '+';
-  const MULTIPLY = '*';
-  const operatorLine = input[input.length - 1];
-  const numberLines = input.slice(0, input.length - 1);
+  const operatorLine = input[input.length - 2];
+  const numberLines = input.slice(0, input.length - 2);
   const operators = operatorLine.match(/[+*]/g);
   if (!operators) {
     throw new Error('Invalid input');
   }
+
   return numberLines
     .reduce((lineTotals, line, lineCount) => {
       const numbers = line.match(/(\d+)/g);
@@ -36,7 +43,50 @@ function part1(input: string[]) {
     .reduce((total, num) => total + num, 0);
 }
 
-function part2(input: string[]) {}
+function part2(input: string[]) {
+  const operatorLine = input[input.length - 2];
+  const numberLines = input.slice(0, input.length - 2);
+  const operators = operatorLine.match(/[+*]/g);
+  if (!operators) {
+    throw new Error('Invalid input');
+  }
+
+  const numberGroups: number[][] = [];
+  let currentGroup: number[] = [];
+  const maxLineLength = Math.max(...numberLines.map((line) => line.length));
+  for (let col = 0; col < maxLineLength; col++) {
+    let currentNumString = '';
+    for (let row = 0; row < numberLines.length; row++) {
+      if (!numberLines[row][col]) {
+        continue;
+      }
+      currentNumString += numberLines[row][col];
+    }
+    if (currentNumString.trim().length === 0) {
+      numberGroups.push(currentGroup);
+      currentGroup = [];
+      continue;
+    }
+    const num = Number.parseInt(currentNumString.trim(), 10);
+
+    currentGroup.push(num);
+  }
+  if (currentGroup.length > 0) {
+    numberGroups.push(currentGroup);
+  }
+
+  return numberGroups.reduce((total, group, index) => {
+    if (operators[index] === PLUS) {
+      const subtotal = group.reduce((acc, num) => acc + num, 0);
+      return total + subtotal;
+    } else if (operators[index] === MULTIPLY) {
+      const product = group.reduce((acc, num) => acc * num, 1);
+      return total + product;
+    } else {
+      throw new Error('Invalid operator!');
+    }
+  }, 0);
+}
 
 function go(): void {
   console.time('task');
