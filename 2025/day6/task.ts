94e7716fea2b0981ccbd53afa0d75f4039e3fe62
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-06 21:34:33 +1100

    2025-day6: done p1

--- 2025/day6/task.ts
+++ 2025/day6/task.ts
@@ -1,6 +1,40 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {}
+function part1(input: string[]) {
+  const PLUS = '+';
+  const MULTIPLY = '*';
+  const operatorLine = input[input.length - 1];
+  const numberLines = input.slice(0, input.length - 1);
+  const operators = operatorLine.match(/[+*]/g);
+  if (!operators) {
+    throw new Error('Invalid input');
+  }
+  return numberLines
+    .reduce((lineTotals, line, lineCount) => {
+      const numbers = line.match(/(\d+)/g);
+      if (!numbers) {
+        return lineTotals;
+      }
+
+      numbers.forEach((numString, numIndex) => {
+        const num = Number.parseInt(numString, 10);
+        if (lineCount === 0) {
+          lineTotals.push(num);
+          return;
+        }
+
+        if (operators[numIndex] === PLUS) {
+          lineTotals[numIndex] += num;
+        } else if (operators[numIndex] === MULTIPLY) {
+          lineTotals[numIndex] *= num;
+        } else {
+          throw new Error('Invalid operator');
+        }
+      });
+      return lineTotals;
+    }, [] as number[])
+    .reduce((total, num) => total + num, 0);
+}
 
 function part2(input: string[]) {}
 
