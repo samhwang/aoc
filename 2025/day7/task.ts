13be8cf043b5320350375a46188b1cbb0e572bf7
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-08 00:03:19 +1100

    2025-day7: Refactor to merge the solutions together into 1 func

--- 2025/day7/task.ts
+++ 2025/day7/task.ts
@@ -1,47 +1,26 @@
 import { parseInput } from '../src/parse-input';
 
-function part1(input: string[]) {
+function solve(input: string[]) {
   const [startLine, ...nextLines] = input;
   const start = startLine.indexOf('S');
+  if (start === -1) {
+    throw new Error('Start not found');
+  }
+
   let beamPositions = [start];
+
+  // For part 1: If a split is encountered, append to this value
   let splitCount = 0;
-  nextLines.forEach((line) => {
-    const matches = [...line.matchAll(/\^/g)];
-    if (matches.length === 0) {
-      return;
-    }
 
-    for (const match of matches) {
-      const splitterIndex = match.index;
-      if (beamPositions.includes(splitterIndex)) {
-        beamPositions = beamPositions.filter((pos) => pos !== splitterIndex);
-        if (splitterIndex > 0) {
-          beamPositions.push(splitterIndex - 1);
-        }
-        if (splitterIndex < line.length - 1) {
-          beamPositions.push(splitterIndex + 1);
-        }
-        splitCount++;
-      }
-    }
-  });
-  return splitCount;
-}
-
-function part2(input: string[]) {
-  const [startLine, ...nextLines] = input;
-  const start = startLine.indexOf('S');
-
-  // How the number of "realities" is calculated:
-  // - Assuming the default weight of an index is 0
+  // For part 2: How the number of "realities" is calculated:
+  // - Assuming the default weight of an index is undefined
   // - If a path goes through it, then its 1
   // - If a path meets a split, then remove said weight from the index, and take the current value and pass it to index + 1 and index - 1
   // - If an index can be hit by multiple splits, add the value from said splits together
   // - After iterating through the whole map, add up all of the "realities" at the end.
-
-  let beamPositions = [start];
   const pathValue = new Map<number, number>();
   pathValue.set(start, 1);
+
   nextLines.forEach((line) => {
     const matches = [...line.matchAll(/\^/g)];
     if (matches.length === 0) {
@@ -52,12 +31,17 @@ function part2(input: string[]) {
       const splitterIndex = match.index;
       if (beamPositions.includes(splitterIndex)) {
         beamPositions = beamPositions.filter((pos) => pos !== splitterIndex);
+
+        // Part 2: Take the current path value from the index...
         const currentValue = pathValue.get(splitterIndex);
         if (!currentValue) {
           throw new Error(`Value not found for index ${splitterIndex}. THERE MUST BE A VALUE HERE!!!`);
         }
+
         if (splitterIndex > 0) {
           beamPositions.push(splitterIndex - 1);
+
+          // Part 2: ...and pass to the left
           const leftValue = pathValue.get(splitterIndex - 1);
           if (!leftValue) {
             pathValue.set(splitterIndex - 1, currentValue);
@@ -67,6 +51,8 @@ function part2(input: string[]) {
         }
         if (splitterIndex < line.length - 1) {
           beamPositions.push(splitterIndex + 1);
+
+          // Part 2: and pass to the right
           const rightValue = pathValue.get(splitterIndex + 1);
           if (!rightValue) {
             pathValue.set(splitterIndex + 1, currentValue);
@@ -74,16 +60,26 @@ function part2(input: string[]) {
             pathValue.set(splitterIndex + 1, rightValue + currentValue);
           }
         }
+
+        // Part 1: Append split count
+        splitCount++;
+
+        // Part 2: clear the current index
         pathValue.delete(splitterIndex);
       }
     }
   });
+
   let paths = 0;
   const values = pathValue.values();
   for (const value of values) {
     paths += value;
   }
-  return paths;
+
+  return {
+    splitCount,
+    paths,
+  };
 }
 
 function go(): void {
@@ -93,15 +89,15 @@ function go(): void {
   const input = parseInput('./input.txt');
   console.timeEnd('parse-input');
 
-  console.time('part 1');
-  const res1 = part1(input);
-  console.log('PART 1: ', res1);
-  console.timeEnd('part 1');
+  console.time('SOLVE');
+  const { splitCount, paths } = solve(input);
+  console.timeEnd('SOLVE');
 
-  console.time('part 2');
-  const res2 = part2(input);
+  const res1 = splitCount;
+  console.log('PART 1: ', res1);
+
+  const res2 = paths;
   console.log('PART 2: ', res2);
-  console.timeEnd('part 2');
 
   console.timeEnd('task');
 }
