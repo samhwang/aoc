4f622624d4d8bbd5043c90187cc5eb4897e33d7b
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-07 23:40:42 +1100

    2025-day7: done p2

--- 2025/day7/task.ts
+++ 2025/day7/task.ts
@@ -29,7 +29,63 @@ function part1(input: string[]) {
   return splitCount;
 }
 
-function part2(input: string[]) {}
+function part2(input: string[]) {
+  const [startLine, ...nextLines] = input;
+  const start = startLine.indexOf('S');
+
+  // How the number of "realities" is calculated:
+  // - Assuming the default weight of an index is 0
+  // - If a path goes through it, then its 1
+  // - If a path meets a split, then remove said weight from the index, and take the current value and pass it to index + 1 and index - 1
+  // - If an index can be hit by multiple splits, add the value from said splits together
+  // - After iterating through the whole map, add up all of the "realities" at the end.
+
+  let beamPositions = [start];
+  const pathValue = new Map<number, number>();
+  pathValue.set(start, 1);
+  nextLines.forEach((line) => {
+    const matches = [...line.matchAll(/\^/g)];
+    if (matches.length === 0) {
+      return;
+    }
+
+    for (const match of matches) {
+      const splitterIndex = match.index;
+      if (beamPositions.includes(splitterIndex)) {
+        beamPositions = beamPositions.filter((pos) => pos !== splitterIndex);
+        const currentValue = pathValue.get(splitterIndex);
+        if (!currentValue) {
+          throw new Error(`Value not found for index ${splitterIndex}. THERE MUST BE A VALUE HERE!!!`);
+        }
+        if (splitterIndex > 0) {
+          beamPositions.push(splitterIndex + 1);
+          const rightValue = pathValue.get(splitterIndex + 1);
+          if (!rightValue) {
+            pathValue.set(splitterIndex + 1, currentValue);
+          } else {
+            pathValue.set(splitterIndex + 1, rightValue + currentValue);
+          }
+        }
+        if (splitterIndex < line.length - 1) {
+          beamPositions.push(splitterIndex - 1);
+          const leftValue = pathValue.get(splitterIndex - 1);
+          if (!leftValue) {
+            pathValue.set(splitterIndex - 1, currentValue);
+          } else {
+            pathValue.set(splitterIndex - 1, leftValue + currentValue);
+          }
+        }
+        pathValue.delete(splitterIndex);
+      }
+    }
+  });
+  let paths = 0;
+  const values = pathValue.values();
+  for (const value of values) {
+    paths += value;
+  }
+  return paths;
+}
 
 function go(): void {
   console.time('task');
