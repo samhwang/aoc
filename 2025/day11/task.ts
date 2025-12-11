ed2b962dbd7bf99c4db7f7ba98b3a270b817e3f6
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-12 07:48:12 +1100

    2025-day11: slight refactor

--- 2025/day11/task.ts
+++ 2025/day11/task.ts
@@ -41,12 +41,6 @@ function findAllPaths(map: Map<string, string[]>, current: string, end: string, 
   return pathCount;
 }
 
-function part1(input: string[]) {
-  const map = buildMap(input);
-
-  return findAllPaths(map, START_SIGNAL, END_SIGNAL, new Set());
-}
-
 function findAllPathsContainPoint(map: Map<string, string[]>, start: string, end: string, requiredPoints: string[]): number {
   if (!map.has(start) && !map.has(end)) {
     return 0;
@@ -96,27 +90,22 @@ function findAllPathsContainPoint(map: Map<string, string[]>, start: string, end
   return traverse(start, 0, new Set<string>());
 }
 
-function part2(input: string[]) {
-  const map = buildMap(input);
-
-  return findAllPathsContainPoint(map, SERVER_SIGNAL, END_SIGNAL, [DAC_SIGNAL, FFT_SIGNAL]);
-}
-
 function go(): void {
   console.time('task');
 
   console.time('parse-input');
   // const input = parseInput('./sample2.txt');
   const input = parseInput('./input.txt');
+  const map = buildMap(input);
   console.timeEnd('parse-input');
 
   console.time('part 1');
-  const res1 = part1(input);
+  const res1 = findAllPaths(map, START_SIGNAL, END_SIGNAL, new Set());
   console.log('PART 1: ', res1);
   console.timeEnd('part 1');
 
   console.time('part 2');
-  const res2 = part2(input);
+  const res2 = findAllPathsContainPoint(map, SERVER_SIGNAL, END_SIGNAL, [DAC_SIGNAL, FFT_SIGNAL]);
   console.log('PART 2: ', res2);
   console.timeEnd('part 2');
 
